1eae7b359e262c80716a9999205ceb69d83005dd
Sorin Cosma <sorin.cosma@codemart.ro>
2024-12-30 13:37:35 +0200

    dummy change

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ function App() {
 
   return (
     <>
+		<h1>TEST TEST TEST</h1>
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
